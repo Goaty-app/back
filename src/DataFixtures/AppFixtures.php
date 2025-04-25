37e5475b9ec9741f2b44b9bcab12e2ddd490a391
Arnaud Gaydamour <arnaud.gaydamour@protonmail.com>
2025-04-25 12:12:51 +0200

    feature(auth): configure auth

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,33 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private Generator $faker;
+    private UserPasswordHasherInterface $userPasswordHasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+        $this->faker = Factory::create('fr_FR');
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setEmail('test@test.fr')
+            ->setPassword($this->userPasswordHasher->hashPassword($user, 'password'))
+            ->setRoles(['ROLE_ADMIN'])
+        ;
+
+        $manager->persist($user);
 
         $manager->flush();
     }
