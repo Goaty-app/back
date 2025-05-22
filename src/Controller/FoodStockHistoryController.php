b4c7b5b7a4b00f6468f864a7cf46819e40c433c3
Arnaud Gaydamour <arnaud.gaydamour@protonmail.com>
2025-05-22 17:03:09 +0200

    feature(foodstockhistory): add foodstockhistory

---
+++ src/Controller/FoodStockHistoryController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\FoodStock;
+use App\Entity\FoodStockHistory;
+use App\Enum\Operation;
+use App\Repository\FoodStockHistoryRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
+
+#[Route('api', name: 'api_food_stock_history_')]
+final class FoodStockHistoryController extends AbstractController
+{
+    #[Route('/v1/food-stock/{foodStock}/food-stock-history', name: 'get_all', methods: ['GET'])]
+    public function getAll(
+        FoodStock $foodStock,
+        FoodStockHistoryRepository $foodStockHistory,
+        SerializerInterface $serializer,
+    ): JsonResponse {
+        $data = $foodStockHistory->findByOwnerFlex('foodStock', $foodStock->getId(), $this->getUser());
+        $jsonData = $serializer->serialize($data, 'json', ['groups' => ['foodStockHistory']]);
+
+        return new JsonResponse($jsonData, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/v1/food-stock-history/{foodStockHistory}', name: 'get', methods: ['GET'])]
+    public function get(
+        FoodStockHistory $foodStockHistory,
+        SerializerInterface $serializer,
+    ): JsonResponse {
+        $jsonData = $serializer->serialize($foodStockHistory, 'json', ['groups' => ['foodStockHistory']]);
+
+        return new JsonResponse($jsonData, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/v1/food-stock/{foodStock}/food-stock-history', name: 'create', methods: ['POST'])]
+    public function create(
+        FoodStock $foodStock,
+        Request $request,
+        UrlGeneratorInterface $urlGenerator,
+        SerializerInterface $serializer,
+        EntityManagerInterface $entityManager,
+        ValidatorInterface $validator,
+        TagAwareCacheInterface $cache,
+    ): JsonResponse {
+        /** @var FoodStockHistory */
+        $foodStockHistory = $serializer->deserialize($request->getContent(), FoodStockHistory::class, 'json');
+
+        $errors = $validator->validate($foodStockHistory);
+
+        if ($errors->count() > 0) {
+            return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
+        }
+
+        match ($foodStockHistory->getOperation()) {
+            Operation::PLUS  => $foodStock->setQuantity($foodStock->getQuantity() + $foodStockHistory->getQuantity()),
+            Operation::MINUS => $foodStock->setQuantity($foodStock->getQuantity() - $foodStockHistory->getQuantity()),
+        };
+
+        $entityManager->persist($foodStock);
+
+        $foodStockHistory->setOwner($this->getUser())
+            ->setCreatedAt(new DateTimeImmutable())
+            ->setFoodStock($foodStock)
+        ;
+
+        $entityManager->persist($foodStockHistory);
+        $entityManager->flush();
+
+        $cache->invalidateTags(['foodStocksCache']);
+
+        $jsonData = $serializer->serialize($foodStockHistory, 'json', ['groups' => ['foodStockHistory']]);
+        $location = $urlGenerator->generate('api_food_stock_history_get', ['foodStockHistory' => $foodStockHistory->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return new JsonResponse($jsonData, Response::HTTP_CREATED, ['location' => $location], true);
+    }
+
+    #[Route('/v1/food-stock-history/{foodStockHistory}', name: 'delete', methods: ['DELETE'])]
+    public function delete(
+        FoodStockHistory $foodStockHistory,
+        EntityManagerInterface $entityManager,
+        TagAwareCacheInterface $cache,
+    ): JsonResponse {
+        $foodStock = $foodStockHistory->getFoodStock();
+
+        match ($foodStockHistory->getOperation()) {
+            Operation::PLUS  => $foodStock->setQuantity($foodStock->getQuantity() - $foodStockHistory->getQuantity()),
+            Operation::MINUS => $foodStock->setQuantity($foodStock->getQuantity() + $foodStockHistory->getQuantity()),
+        };
+
+        $entityManager->persist($foodStock);
+
+        $entityManager->remove($foodStockHistory);
+        $entityManager->flush();
+
+        $cache->invalidateTags(['foodStocksCache']);
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+}
---
+++ src/Enum/Operation.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enum;
+
+enum Operation: string
+{
+    case PLUS = '+';
+    case MINUS = '-';
+}
