e35f87e0db295b6a800d344c17741abf1fd67cf9
Arnaud Gaydamour <arnaud.gaydamour@protonmail.com>
2025-05-22 10:40:45 +0200

    feature(production): add production

---
+++ src/Service/ProductionService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Herd;
+use App\Entity\Production;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class ProductionService
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function updateHerd(Production $production, Request $request, UserInterface $currentUser): void
+    {
+        $requestData = json_decode($request->getContent(), true);
+        $herdId = $requestData['herd_id'];
+
+        if (!$herdId) {
+            return;
+        }
+
+        /** @var Herd */
+        $herd = $this->entityManager->getRepository(Herd::class)->find($herdId);
+
+        if (!$herd) {
+            throw new NotFoundHttpException();
+        }
+
+        if ($herd->getOwner() !== $currentUser) {
+            throw new NotFoundHttpException();
+        }
+
+        $production->setHerd($herd);
+    }
+}
