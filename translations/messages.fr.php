5d2b646b845df2f2f73426f327e5bbbac9529c08
Arnaud Gaydamour <arnaud.gaydamour@protonmail.com>
2025-05-31 22:34:23 +0200

    feature(exception): add violations in error message

---
+++ translations/messages.fr.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'exception.unprocessable.entity' => 'Oops ! Il y a un problème avec les informations que vous avez saisies. Veuillez vérifier et corriger les erreurs.',
+    'exception.internal.server.error' => "Un écureuil s'est glissé dans nos câbles ! Nos techniciens le chassent avec des noisettes. 🐿️⚡",
+];
