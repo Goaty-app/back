1fad2637ae4e3d82b02042eec9ce9a3f60118d92
Arnaud Gaydamour <arnaud.gaydamour@protonmail.com>
2025-06-11 10:26:26 +0200

    feature(tests): add tests for register route

--- tests/Func/AuthTest.php
+++ tests/Func/AuthTest.php
@@ -61,4 +61,83 @@ class AuthTest extends WebTestCase
         $responseData = json_decode($client->getResponse()->getContent(), true);
         $this->assertArrayHasKey('message', $responseData);
     }
+
+    public function testRegisterCheckSuccessful(): void
+    {
+        $client = static::createClient();
+
+        $credentials = [
+            'email'    => 'alberto@example.com',
+            'password' => 'JeSuisUnSuperMot980DePasse/',
+        ];
+
+        $client->request(
+            'POST',
+            '/api/register',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode($credentials),
+        );
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_CREATED);
+        $this->assertResponseHeaderSame('content-type', 'application/json');
+
+        $responseData = json_decode($client->getResponse()->getContent(), true);
+
+        $this->assertArrayHasKey('email', $responseData);
+        $this->assertNotEmpty($responseData['email']);
+    }
+
+    public function testRegisterFailedWithExistingEmail(): void
+    {
+        $client = static::createClient();
+
+        $credentials = [
+            'email'    => 'admin@example.com',
+            'password' => 'JeSuisUnSuperMot980DePasse/',
+        ];
+
+        $client->request(
+            'POST',
+            '/api/register',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode($credentials),
+        );
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $this->assertResponseHeaderSame('content-type', 'application/json');
+
+        $responseData = json_decode($client->getResponse()->getContent(), true);
+        $this->assertArrayHasKey('message', $responseData);
+        $this->assertArrayHasKey('violations', $responseData);
+    }
+
+    public function testRegisterFailedWithWeakPassword(): void
+    {
+        $client = static::createClient();
+
+        $credentials = [
+            'email'    => 'admin@example.com',
+            'password' => 'password',
+        ];
+
+        $client->request(
+            'POST',
+            '/api/register',
+            [],
+            [],
+            ['CONTENT_TYPE' => 'application/json'],
+            json_encode($credentials),
+        );
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_UNPROCESSABLE_ENTITY);
+        $this->assertResponseHeaderSame('content-type', 'application/json');
+
+        $responseData = json_decode($client->getResponse()->getContent(), true);
+        $this->assertArrayHasKey('message', $responseData);
+        $this->assertArrayHasKey('violations', $responseData);
+    }
 }
